75898d672d011810528dda9debcd54afa06c301f
Felipe8581 <58223527+Felipe8581@users.noreply.github.com>
2020-09-17 16:31:32 -0700

    Update Tweaks-for-Gaming.bat

--- Tweaks-for-Gaming.bat
+++ Tweaks-for-Gaming.bat
@@ -65,9 +65,9 @@ dism /online /enable-feature /featurename:DirectPlay /all /norestart >NUL 2>&1
 dism /online /enable-feature /featurename:NetFx4-AdvSrvs /all /norestart >NUL 2>&1
 dism /online /enable-feature /featurename:NetFx3 /all /norestart >NUL 2>&1
 
-ECHO Enabling MSI for GPU...
+ECHO  Enabling MSI for GPU...
 for /f %%g in ('wmic path win32_videocontroller get PNPDeviceID ^| findstr /L "VEN_"') do (
-REG ADD "HKLM\SYSTEM\CurrentControlSet\Enum\%%g\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f
+REG ADD "HKLM\SYSTEM\CurrentControlSet\Enum\%%g\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f >NUL 2>&1
 )
 
 ECHO  Disabling Mitigations...
