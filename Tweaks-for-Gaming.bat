6bd27ea7a769f707277aaa058e7cfa8f4d038aae
Felipe8581 <58223527+Felipe8581@users.noreply.github.com>
2020-09-19 13:19:36 -0700

    Update Tweaks-for-Gaming.bat

--- Tweaks-for-Gaming.bat
+++ Tweaks-for-Gaming.bat
@@ -11,18 +11,59 @@ echo  Automatization is not the best way, please learn tweaking
 echo  You can start reading all guides on Revision discord (revi.cc)
 echo.
 
-::  Automatically set static ip, Thanks to Phlegm
-if "%INTERFACE%"=="" for /F "tokens=3,*" %%i in ('netsh int show interface^|find "Connected"') do set INTERFACE=%%j
-if "%IP%"=="" for /F "tokens=3 delims=: " %%i in ('netsh int ip show config name^="%INTERFACE%" ^| findstr "IP Address" ^| findstr [0-9]') do set IP=%%i
-if "%MASK%"=="" for /F "tokens=2 delims=()" %%i in ('netsh int ip show config name^="%INTERFACE%" ^| findstr /r "(.*)"') do for %%j in (%%i) do set MASK=%%j
-if "%GATEWAY%"=="" for /F "tokens=3 delims=: " %%i in ('netsh int ip show config name^="%INTERFACE%" ^| findstr "Default" ^| findstr [0-9]') do set GATEWAY=%%i
+:: Check for admin Privileges
+whoami /groups | find "12288" >nul 2>&1
+if %errorlevel% neq 0 (
+echo This batch file must be Run as Administrator.
+pause
+exit /b 1
+)
+
+:: Automatically set static ip, Thanks to Phlegm
 set DNS1=156.154.70.22
 set DNS2=8.8.4.4
-netsh int ipv4 set address name="%INTERFACE%" static %IP% %MASK% %GATEWAY% >NUL 2>&1
-netsh int ipv4 set dns name="%INTERFACE%" static %DNS1% primary >NUL 2>&1
-netsh int ipv4 add dns name="%INTERFACE%" %DNS2% index=2 >NUL 2>&1
-netsh int set interface name="%INTERFACE%" admin="disabled" && netsh int set interface name="%INTERFACE%" admin="enabled" >NUL 2>&1
-
+if "%INTERFACE%"=="" for /f "tokens=3,*" %%i in ('netsh int show interface^|find "Connected"') do set INTERFACE=%%j
+if "%IP%"=="" for /f "tokens=3 delims=: " %%i in ('netsh int ip show config name^="%INTERFACE%" ^| findstr "IP Address" ^| findstr [0-9]') do set IP=%%i
+if "%MASK%"=="" for /f "tokens=2 delims=()" %%i in ('netsh int ip show config name^="%INTERFACE%" ^| findstr /r "(.*)"') do for %%j in (%%i) do set MASK=%%j
+if "%GATEWAY%"=="" for /f "tokens=3 delims=: " %%i in ('netsh int ip show config name^="%INTERFACE%" ^| findstr "Default" ^| findstr [0-9]') do set GATEWAY=%%i
+if "%DNS1%"=="" for /f "tokens=2 delims=: " %%i in ('echo quit^|nslookup^|find "Address:"') do set DNS1=%%i
+call:isValidIP %IP%
+call:isValidIP %MASK%
+call:isValidIP %GATEWAY%
+call:isValidIP %DNS1%
+if defined DNS2 call:isValidIP %DNS2%
+if defined _notValidIP (
+echo Setting a static IP failed. Exiting program . . .
+pause
+exit /b 1
+)
+netsh int ipv4 set address name="%INTERFACE%" static %IP% %MASK% %GATEWAY% >nul 2>&1
+netsh int ipv4 set dns name="%INTERFACE%" static %DNS1% primary >nul 2>&1
+if defined DNS2 netsh int ipv4 add dns name="%INTERFACE%" %DNS2% index=2 >nul 2>&1
+for /f "tokens=3 delims=: " %%i in ('netsh int ip show config name^="%INTERFACE%" ^| findstr "DHCP" ^| findstr [a-z]') do set DHCP=%%i
+if "%DHCP%"=="Yes" (
+echo Setting a static IP failed. Exiting program . . .
+pause
+exit /b 2
+) else (
+echo Restarting the Network Adapter . . .
+netsh int set interface name="%INTERFACE%" admin="disabled" && netsh int set interface name="%INTERFACE%" admin="enabled" >nul 2>&1
+echo Done.
+echo.
+pause
+exit /b 0
+)
+:isValidIP
+for /F "tokens=1-4 delims=./" %%a in ("%1") do (
+	if %%a LSS 1 set _notValidIP=1
+	if %%a GTR 255 set _notValidIP=1
+	if %%b LSS 0 set _notValidIP=1
+	if %%b GTR 255 set _notValidIP=1
+	if %%c LSS 0 set _notValidIP=1
+	if %%c GTR 255 set _notValidIP=1
+	if %%d LSS 0 set _notValidIP=1
+	if %%d GTR 255 set _notValidIP=1
+)
 
 echo  Preparation, removing protections and starting services
 :: ExecutionPolicy,UAC,WinUpdate,WinDefender
