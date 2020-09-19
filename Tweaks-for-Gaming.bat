4924e6b601a04609aaeb55a702920d4726e1687a
Felipe8581 <58223527+Felipe8581@users.noreply.github.com>
2020-09-19 03:22:49 -0300

    Update Tweaks-for-Gaming.bat

--- Tweaks-for-Gaming.bat
+++ Tweaks-for-Gaming.bat
@@ -14,12 +14,12 @@ ECHO.
 
 :: Enabling and starting required services...
 POWERSHELL "Set-ExecutionPolicy -ExecutionPolicy Unrestricted" >NUL 2>&1
-SC CONFIG Winmgmt start=demand & SC START Winmgmt >NUL 2>&1
-SC CONFIG TrustedInstaller start=demand & SC START TrustedInstaller >NUL 2>&1
-SC CONFIG AppInfo start=demand & SC START AppInfo >NUL 2>&1
-SC CONFIG DeviceInstall start=demand & SC START DeviceInstall >NUL 2>&1
-SC CONFIG Dhcp start=demand & SC START Dhcp >NUL 2>&1
-SC CONFIG w32time start=demand & SC START w32time >NUL 2>&1
+SC CONFIG Winmgmt start=demand >NUL 2>&1 & SC START Winmgmt >NUL 2>&1
+SC CONFIG TrustedInstaller start=demand >NUL 2>&1 & SC START TrustedInstaller >NUL 2>&1
+SC CONFIG AppInfo start=demand >NUL 2>&1 & SC START AppInfo >NUL 2>&1
+SC CONFIG DeviceInstall start=demand >NUL 2>&1 & SC START DeviceInstall >NUL 2>&1
+SC CONFIG Dhcp start=demand >NUL 2>&1 & SC START Dhcp >NUL 2>&1
+SC CONFIG w32time start=demand >NUL 2>&1 & SC START w32time >NUL 2>&1
 
 :: Resync time
 w32tm /config /manualpeerlist:time.windows.com >NUL 2>&1
